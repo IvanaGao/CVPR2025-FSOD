08479ad509bbceb40700a32df416f9282b45f88c
IvanaGao <gaoivana2002@gmail.com>
2025-11-09 15:37:02 +0800

    Add files via upload

---
+++ models/OVDino/__init__.py
@@ -0,0 +1,9 @@
+# from .ovdino_grounding_tiny import build_ovdino_grounding_tiny
+# from .ovdino_grounding_tiny_A0 import build_ovdino_grounding_tiny_A0
+# from .ovdino_grounding_tiny_C0 import build_ovdino_grounding_tiny_C0
+
+from .ovdino_grounding_base import build_ovdino_grounding_base
+# from .ovdino_grounding_base_V0 import build_ovdino_grounding_base_V0
+
+# from .ovdino_grounding_large import build_ovdino_grounding_large
+
---
+++ models/OVDino/iou.py
@@ -0,0 +1,38 @@
+# GRADED FUNCTION: iou
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+
+    Arguments:
+    box1 -- first box, list object with coordinates (box1_x1, box1_y1, box1_x2, box_1_y2)
+    box2 -- second box, list object with coordinates (box2_x1, box2_y1, box2_x2, box2_y2)
+    """
+
+    # Assign variable names to coordinates for clarity
+    (box1_x1, box1_y1, box1_x2, box1_y2) = box1
+    (box2_x1, box2_y1, box2_x2, box2_y2) = box2
+
+    # Calculate the (yi1, xi1, yi2, xi2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    ### START CODE HERE ### (≈ 7 lines)
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_width = xi2 - xi1
+    inter_height = yi2 - yi1
+    inter_area = max(inter_width, 0) * max(inter_height, 0)
+    ### END CODE HERE ###
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    ### START CODE HERE ### (≈ 3 lines)
+    box1_area = (box1[3] - box1[1]) * (box1[2] - box1[0])
+    box2_area = (box2[3] - box2[1]) * (box2[2] - box2[0])
+    union_area = box1_area + box2_area - inter_area
+    ### END CODE HERE ###
+
+    # compute the IoU
+    ### START CODE HERE ### (≈ 1 line)
+    iou = float(inter_area) / float(union_area)
+    ### END CODE HERE ###
+
+    return iou
